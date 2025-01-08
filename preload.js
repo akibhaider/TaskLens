9ee37b1f8f0d7f0126714be4f616a9b54792002c
Akib Haider <akibhaider@iut-dhaka.edu>
2025-01-09 05:11:43 +0600

    electron forge dev dependency setup

---
+++ preload.js
@@ -0,0 +1,16 @@
+/// A preload script attachment to renderer. It runs before the preload script is loaded
+/// Has access to both renderer globals (e.g. window and document) and a Node.js environment
+// preload.js
+
+// All the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  })
